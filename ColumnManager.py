4c5f2e8aee21d64729937fc597afbb4343734cb9
pwwpche <pwwpche@gmail.com>
2017-06-05 20:34:54 -0700

    update column_manger

--- ColumnManager.py
+++ ColumnManager.py
@@ -1,47 +1,53 @@
 # -*- coding: utf-8 -*-
 
 import SSTable
-class ColumnManager():
+
+class ColumnManager:
+
     def __init__(self, tableID,column_name):
-        self.filename=str(tableID)+'_'+column_name
+        self.filename = str(tableID)+'_'+column_name
         self.mem = {}
         self.imTable = []
-        self.maxLength=8
-        self.ssTable=SSTable()
+        self.maxLength = 8
+        self.ssTable = SSTable()
+        self.now = -1
+        self.fileNumber = self.ssTable.getnumber()
+        self.imTableNumber = len(self.imTable) + self.fileNumber
+
     def get(self, key):
         if key in self.mem:
             return self.mem[key]
-        for i in range(reversed(len(self.imTable))):
+        for i in range(len(self.imTable)):
             if key in self.imTable[i]:
                 return self.imTable[i][key]
-        return self.ssTable.searchDisk(key)
+        return self.ssTable.get(key)
 
     def put(self, key, value):
-        self.mem[key]=value
-        if len(self.mem)>self.maxLength:
+        self.mem[key] = value
+
+        if len(self.mem) > self.maxLength:
             self.imTable.append(self.mem)
-            self.mem={}
-        if len(self.imTable)>self.maxLength:
-            file=dict()
-            for i in range(reversed(len(self.imTable))):
+            self.mem = {}
+
+        if len(self.imTable) > self.maxLength:
+            totalDict = dict()
+            for i in range(len(self.imTable)):
                 for key in self.imTable[i]:
-                    if key not in file:
-                        file[key]=self.imTable[i][key]
-            self.ssTable.store(file)
-            self.imTable=[]
+                    totalDict[key] = self.imTable[i][key]
+
+            self.ssTable.store(totalDict)
+            self.imTable = []
+
     def remove(self, key):
-        self.mem[key]=None
-    def getInit(self):
-        self.now=-1
-        self.filenumber=self.ssTable.getnumber()
-        self.imtablenumber=len(self.imTable)+self.filenumber
+        self.mem[key] = None
+
     def getlist(self):
-        self.now+=1
-        if self.now<self.filenumber:
+        self.now += 1
+        if self.now < self.fileNumber:
             return self.ssTable.getfile(self.now)
-        elif self.now<self.imtablenumber:
-            return self.imtable[self.now-self.filenumber]
-        elif self.now==self.imtablenumber:
+        elif self.now < self.imTableNumber:
+            return self.imtable[self.now - self.fileNumber]
+        elif self.now == self.imTableNumber:
             return self.mem
         else:
             return None
